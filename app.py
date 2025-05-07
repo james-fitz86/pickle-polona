36fbc176a16f68ba19629872c5c0aad9b2eb52ab
James Fitzpatrick <james.fitzy86y@gmail.com>
2025-05-07 13:38:23 +0100

    added blueprints for the routes

--- app.py
+++ app.py
@@ -1,27 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask
+from routes import blueprints
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    """Home page for the app"""
-    return render_template("index.html")
-
-@app.route("/store")
-def store():
-    """Storefront page for the app"""
-    return render_template("store.html")
-
-@app.route("/about")
-def about():
-    """About page for the app"""
-    return render_template("about.html")
-
-@app.route("/contact")
-def contact():
-    """Contact page for the app"""
-    return render_template("contact.html")
-
+for bp in blueprints:
+    app.register_blueprint(bp)
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
