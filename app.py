c6052d7f420fbe1b261944bf5869936260f344dc
James Fitzpatrick <james.fitzy86y@gmail.com>
2025-05-10 16:27:43 +0100

    moved db init to extensions and updated app.py to avoid circular import

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
 from flask import Flask
+from extensions import db
 from routes import blueprints
 from dotenv import load_dotenv
-from flask_sqlalchemy import SQLAlchemy
-from config import Config
-
-app = Flask(__name__)
-app.config.from_object(Config)
-
-db = SQLAlchemy(app)
 
 
-for bp in blueprints:
-    app.register_blueprint(bp)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("config.Config")
+
+    db.init_app(app)
+
+
+    for bp in blueprints:
+        app.register_blueprint(bp)
+    
+    return app
+
+app = create_app()
+
+with app.app_context():
+    from models.product import Product
+    db.create_all() 
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
