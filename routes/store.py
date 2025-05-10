ed3aa5609736e21ccc1be1c9f0e28aac2e1af198
James Fitzpatrick <james.fitzy86y@gmail.com>
2025-05-10 16:33:00 +0100

    altered route to fetch product data for display on store front

--- routes/store.py
+++ routes/store.py
@@ -1,8 +1,14 @@
 from flask import Blueprint, render_template
+from sqlalchemy import select
+from models.product import Product
+from extensions import db
 
 store = Blueprint('store', __name__)
 
 @store.route("/store")
 def storefront():
     """Storefront page for the app"""
-    return render_template("store.html")
+    stmt = select(Product)
+    print(stmt, flush=True)
+    products = db.session.scalars(stmt)
+    return render_template("store.html", products=products)
