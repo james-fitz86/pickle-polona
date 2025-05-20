d971960a12c07025833f97448b0020f9a30b5a15
James Fitzpatrick <james.fitzy86y@gmail.com>
2025-05-20 13:38:53 +0100

    altered form to match changes to Product model class

--- forms/product_form.py
+++ forms/product_form.py
@@ -17,12 +17,8 @@ class ProductForm(FlaskForm):
     image_main = StringField("Main Image Filename", validators=[Optional(), Length(max=200)])
     image_1 = StringField("Image 1 Filename", validators=[Optional(), Length(max=200)])
     image_2 = StringField("Image 2 Filename", validators=[Optional(), Length(max=200)])
-
-    stock_quantity = IntegerField("Stock Quantity", validators=[InputRequired()])
+    
     in_stock = BooleanField("In Stock", default=True)
     packs_per_box = IntegerField("Packs Per Box", validators=[InputRequired()])
-
-    expiry_date = DateField("Expiry Date", format='%Y-%m-%d', validators=[Optional()])
-    stock_location = StringField("Location", validators=[Optional(), Length(max=100)])
 
     submit = SubmitField("Add Product")
