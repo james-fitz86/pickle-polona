48e66858b72a96a8ccdd3d091cbdaf0c95ace27a
James Fitzpatrick <james.fitzy86y@gmail.com>
2025-05-20 13:33:54 +0100

    added form for adding stock batch

---
+++ forms/stock_form.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, StringField, DateField, SubmitField, SelectField
+from wtforms.validators import InputRequired, Optional, Length
+
+class StockForm(FlaskForm):
+    stock_quantity = IntegerField("Stock Quantity", validators=[InputRequired()])
+    stock_location = StringField("Stock Location", validators=[Optional(), Length(max=100)])
+    expiry_date = DateField("Expiry Date", format='%Y-%m-%d', validators=[Optional()])
+    product_sku = SelectField("Product SKU", choices=[], validators=[InputRequired()])
+
+    submit = SubmitField("Add Stock")
