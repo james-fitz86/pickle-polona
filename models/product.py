f8719388573f071c8292912c742ffcc18ac260bf
James Fitzpatrick <james.fitzy86y@gmail.com>
2025-05-20 13:34:36 +0100

    altered Prodict class model and added Batch class model

--- models/product.py
+++ models/product.py
@@ -20,15 +20,26 @@ class Product(db.Model):
     image_1 = db.mapped_column(db.String(200))
     image_2 = db.mapped_column(db.String(200))
 
-    stock_quantity = db.mapped_column(db.Integer, nullable=False, default=0)
     in_stock = db.mapped_column(db.Boolean, default=True)
 
-    expiry_date = db.mapped_column(db.Date)
-    stock_location = db.mapped_column(db.String(100))
     packs_per_box = db.mapped_column(db.Integer, nullable=False, default=0)
 
     created_at = db.mapped_column(db.DateTime, default=datetime.utcnow)
     updated_at = db.mapped_column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
+    batches = db.relationship('Batch', backref='product', lazy=True)
+
     def __repr__(self):
         return f"<Product {self.sku} - {self.name}>"
+    
+class Batch(db.Model):
+    __tablename__ = 'batches'
+
+    id = db.Column(db.Integer, primary_key=True)
+    stock_quantity = db.mapped_column(db.Integer, nullable=False, default=0)
+    stock_location = db.mapped_column(db.String(100))
+    expiry_date = db.mapped_column(db.Date)
+    product_sku = db.mapped_column(db.String, db.ForeignKey('products.sku'), nullable=False)
+
+    def __repr__(self):
+        return f"<Batch {self.id} - SKU: {self.product_sku}, Qty: {self.stock_quantity}>"
