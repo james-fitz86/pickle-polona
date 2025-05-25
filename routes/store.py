51ead71d3028f6bde3bd243961e345319fb22186
James Fitzpatrick <james.fitzy86y@gmail.com>
2025-05-25 17:07:20 +0100

    added route for cart'

--- routes/store.py
+++ routes/store.py
@@ -14,6 +14,12 @@ def storefront():
 
 @store.route("/item/<string:product_sku>")
 def item(product_sku):
+    """Indivual Product page"""
     stmt = select(Product).where(Product.sku == product_sku)
     product = db.session.scalars(stmt).first()
     return render_template("item.html", product=product)
+
+@store.route("/cart")
+def cart():
+    """Shopping Cart page for the app"""
+    return render_template("cart.html")
