ad6d30d50df4b73b0864789ee0f098f6b3e3cacb
James Fitzpatrick <james.fitzy86y@gmail.com>
2025-05-26 21:12:32 +0100

    added price, maxStock & subtotal to cart array

--- static/js/store.js
+++ static/js/store.js
@@ -6,16 +6,26 @@ function addToCart(){
     const input = document.querySelector(".quantity-input");
     const quantity = parseInt(input.value);
     const sku = input.dataset.productSku;
+    const price = parseFloat(input.dataset.price);
     const name = input.dataset.productName
     const image = input.dataset.productImage
+    const maxStock = parseInt(input.dataset.maxStock)
+    const subtotal = price * quantity
     let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
     const existingItem = cart.find(item => item.sku === sku);
+    const currentQuantity = existingItem ? parseInt(existingItem.quantity) : 0;
+
+    if (currentQuantity + quantity > maxStock) {
+        alert(`Max quantity of ${name} is ${maxStock}`);
+        return;
+    }
     
     if (existingItem){
         existingItem.quantity = (parseInt(existingItem.quantity)) + quantity;
     } else {
-        cart.push({sku, name, quantity, image});
+        cart.push({sku, name, quantity, image, maxStock, price, subtotal});
+        console.log(price);
     }
 
     localStorage.setItem("cart", JSON.stringify(cart));
