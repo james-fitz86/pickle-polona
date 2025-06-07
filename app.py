0c69904b6e0bc6c6495510a361469dee4a71faba
James Fitzpatrick <james.fitzy86y@gmail.com>
2025-06-07 15:16:22 +0100

    removed local run of app for render

--- app.py
+++ app.py
@@ -21,10 +21,3 @@ def create_app():
     return app
 
 app = create_app()
-
-with app.app_context():
-    from models.product import Product
-    db.create_all() 
-
-if __name__ == "__main__":
-    app.run(debug=True, port=8080)
